ee1233a62ac2763e683cedd62aa20045d2f0eea4
mathurninaad <mathurninaad@gmail.com>
2024-12-15 02:28:46 +0530

    Add login functionality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import csv
 import base64
-from fileinput import close
 
 def encode(string: str):
     string_to_byte = string.encode('utf-8')
@@ -23,18 +22,54 @@ def check_username(username: str, file: list):
         return False, "The username already exists"
     return True, "Success"
 
+def check_password(password: str):
+    if len(password) < 8:
+        return False, "The length of the password should be more than eight letters"
+    return True, "Success"
 
+def find_username(user_name, file):
+    for line in file:
+        if decode(line['user_name']) == user_name:
+            return [decode(line['user_name']), decode(line['password']), line['status']]
+    return None
+
+login = False
 while True:
-    print("Hello there!, Would you like to login or Sign-up.")
-    print("1 for login")
-    print("2 for signup")
+    if login is False:
+        print("Hello there!, Would you like to login or Sign-up.")
+        print("1 for login")
+        print("2 for signup")
+        print("3 to exit")
+    else:
+        print("Press 1 to logout!")
 
     user_inp = int(input())
     if user_inp == 1:
-        with open('details.csv' , 'r') as details:
-            reader = csv.DictReader(details)
+        if login:
+            print("Signing out! Hope to see you soon!")
+            login = False
+        else:
+            with open('details.csv' , 'r') as details:
+                reader = csv.DictReader(details)
+                user_name = input("Please enter your username: ")
+                user = None
+                while True:
+                    if user_name == '':
+                        print("Please enter a valid username: ")
+                    else:
+                        file = list()
+                        for line in reader:
+                            file.append({'user_name': line['user_name'], 'password': line['password'], 'status': line['status']})
+                        user = find_username((user_name), file)
+                        break
+                password = input("Please enter your password: ")
 
-    elif user_inp == 2:
+                if (user is not None and user[0] == user_name and user[1] == password):
+                    login = True
+                    print(f"Welcome {user_name}!")
+                else:
+                    print("Invalid Credentials!")
+    elif user_inp == 2 and not login:
         l = list()
         with open('details.csv', 'r') as det:
             reader = csv.DictReader(det)
@@ -44,9 +79,6 @@ while True:
 
         with open('details.csv', 'a+') as details:
             writer = csv.DictWriter(details, fieldnames=['user_name', 'password', 'status'])
-            for i in l:
-                print(i)
-
             while True:
                 user_name = input("Please enter your username: ")
                 suc, mess = check_username(user_name, l)
@@ -56,20 +88,29 @@ while True:
                     print(mess)
 
 
-            password = input("Please enter your password: ")
+            while True:
+                password = input("Please enter your password: ")
+                suc, msg = check_password(password)
+                if suc:
+                    break
+                else:
+                    print(msg)
+
             confirm_password = input("Please confirm your password: ")
 
-            if password == confirm_password:
-                status = input("Please enter a status: ")
-                dic = dict()
-                dic['user_name'] = encode(user_name)
-                dic['password'] = encode(password)
-                dic['status'] = status
-                writer.writerow(dic)
-            else:
-                print("Wrong password!!!")
+            while True:
+                if password == confirm_password:
+                    status = input("Please enter a status: ")
+                    dic = dict()
+                    dic['user_name'] = encode(user_name)
+                    dic['password'] = encode(password)
+                    dic['status'] = status
+                    writer.writerow(dic)
+                    break
+                else:
+                    print("Wrong password!!!")
 
-
-
+    elif (user_inp == 3 and not login):
+        break
     else:
         print("Please enter a valid value: ")
